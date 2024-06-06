b0c2f1c397be10250c7971ede934f5afbc74a230
ufard100 <ufuk.arda@hhu.de>
2024-06-06 18:40:55 +0200

    Initial commit

---
+++ SpringChatStore/src/main/java/com/horizon/SpringChatStore/SpringChatStoreApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.SpringChatStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatStoreApplication.class, args);
+	}
+
+}
